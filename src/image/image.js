22e042b9e8e7b462bb493a8cb37a9567fdf04264
jonny <jonathanheindl@gmx.de>
2024-05-14 09:37:28 +0200

    added image/png base64 encoding + matcher

--- src/image/image.js
+++ src/image/image.js
@@ -10,13 +10,15 @@ export const imageConversions = [
         nameHTML: 'image',
         key: 'image',
         matcherPrio: 10,
-        matcher: str => str.startsWith("/9j/"),
+        matcher: str => str.startsWith("/9j/") || str.startsWith("iVBORw"),
         fnc: (str, el, out) => {
             const currentOptions = out.currentParameter.options.operation || "preview"
             currentImage?.remove()
             if(currentOptions === "preview") {
                 if(str.startsWith("/9j/")) {
                     str = `data:image/jpeg;base64,${str}`
+                } else if(str.startsWith("iVBORw")) {
+                    str = `data:image/png;base64,${str}`
                 }
 
 
